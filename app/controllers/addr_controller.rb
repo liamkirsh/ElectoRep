a9237bbfa7e09aa2d42d0b9e487f104c72dd453f
Liam Kirsh <liam.kirsh@riseup.net>
2015-06-28 01:36:30 -0700

    trying to pass data to html

--- app/controllers/addr_controller.rb
+++ app/controllers/addr_controller.rb
@@ -1,6 +1,24 @@
 class AddrController < ApplicationController
+    require 'open-uri'
+    
 	def submit
 		puts params[:address]
-		redirect_to root_url
+        @address = params[:address]
+    url = "https://www.googleapis.com/civicinfo/v2/representatives?address=" + @address + "&includeOffices=true&levels=country&roles=legislatorLowerBody&roles=legislatorUpperBody&fields=normalizedInput%2Cofficials&key=" + ENV["google_dev_key"]
+    url = url.gsub(' ', '+')
+    doc = Nokogiri::HTML(open(url))
+    json = JSON.parse(doc)
+    json["officials"].each do |official|
+        print official["name"] + "\n"
+        print official["address"][0]["line1"] + "\n"
+        print official["address"][0]["city"].titlecase + ", "\
+          + official["address"][0]["state"] + " "\
+          + official["address"][0]["zip"] + "\n"
+        print official["phones"][0]
+        #official["photoUrl"]
+        print "\n\n"
+        end
+        
+        redirect_to root_url
 	end
 end
