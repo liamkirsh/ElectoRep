29a735e5e7804ff790c44dcdcf696620a24063c5
Liam Kirsh <liam.kirsh@riseup.net>
2015-06-27 21:36:51 -0700

    removed unnecessary file

--- app/controllers/nokogiri.rb
+++
@@ -1,9 +0,0 @@
-require 'rubygems'
-require 'nokogiri'
-require 'open-uri'
-
-url = "http://ontheissues.org/Senate/Dianne_Feinstein.htm"
-doc = Nokogiri::HTML(open(url))
-#puts doc.at_css("title").text
-abortion = doc.at_xpath('//ul[preceding::a[@id="Drugs"]]')
-puts abortion
