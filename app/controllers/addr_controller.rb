c6cddd8efb2c175fbdd1920b54331f90dfcead8c
Liam Kirsh <liam.kirsh@riseup.net>
2015-06-28 05:10:41 -0700

    come at me bro

--- app/controllers/addr_controller.rb
+++ app/controllers/addr_controller.rb
@@ -4,21 +4,21 @@ class AddrController < ApplicationController
 	def submit
 		puts params[:address]
         @address = params[:address]
-    url = "https://www.googleapis.com/civicinfo/v2/representatives?address=" + @address + "&includeOffices=true&levels=country&roles=legislatorLowerBody&roles=legislatorUpperBody&fields=normalizedInput%2Cofficials&key=" + ENV["google_dev_key"]
-    url = url.gsub(' ', '+')
-    doc = Nokogiri::HTML(open(url))
-    json = JSON.parse(doc)
-    json["officials"].each do |official|
-        print official["name"] + "\n"
-        print official["address"][0]["line1"] + "\n"
-        print official["address"][0]["city"].titlecase + ", "\
-          + official["address"][0]["state"] + " "\
-          + official["address"][0]["zip"] + "\n"
-        print official["phones"][0]
-        #official["photoUrl"]
-        print "\n\n"
-        end
+        url = "https://www.googleapis.com/civicinfo/v2/representatives?address=" + @address + "&includeOffices=true&levels=country&roles=legislatorLowerBody&roles=legislatorUpperBody&fields=normalizedInput%2Cofficials&key=" + ENV["google_dev_key"]
+        url = url.gsub(' ', '+')
+        doc = Nokogiri::HTML(open(url))
+        json = JSON.parse(doc)
+        'json["officials"].each do |official|
+            print official["name"] + "\n"
+            print official["address"][0]["line1"] + "\n"
+            print official["address"][0]["city"].titlecase + ", "\
+              + official["address"][0]["state"] + " "\
+              + official["address"][0]["zip"] + "\n"
+            print official["phones"][0]
+            #official["photoUrl"]
+            print "\n\n"
+        end'
         
-        redirect_to root_url
+        render :text => {:msg => json}
 	end
 end
